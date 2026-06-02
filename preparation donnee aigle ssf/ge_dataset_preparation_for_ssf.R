4666d000bd9edb01fe7ef021e4ca3a33fb8c67fd
LouiseFau <faurelouise91@yahoo.fr>
2026-06-02 15:59:13 +0200

    remove weird location points (altitude)

--- preparation donnee aigle ssf/ge_dataset_preparation_for_ssf.R
+++ preparation donnee aigle ssf/ge_dataset_preparation_for_ssf.R
@@ -6,57 +6,52 @@
 #' (ii) define a threshold value to keep the location close to the group
 #' (iii) identify the time at which each location become independent    
 #' ---   
-#' 
+
+
 #' #library
 library(dplyr)
-
-
-
-
+library(terra)
 
 #' # STEP 0 : load the data ----------------------------------------------------
-#' # set output directory 
 output_dir <- "C:/Users/lfaure7/Documents/git/chapter-2/preparation donnee aigle ssf/resultats-intermediaire"
 
-# Emigration date
+# Géoïde and digital elevation model
+geo <- terra::rast("C:/Users/lfaure7/OneDrive/MEMOIRE M2/eagle_projet/data/geoide/us_nga_egm96_15.tif")
+dem <- terra::rast("C:/Users/lfaure7/OneDrive/MEMOIRE M2/eagle_projet/data/pretraitements/Region-Alpes-Dem/region-alpes-dem.tif")
+
+# emigration dates and golden eagle data
 emig_dates <- readRDS("C:/Users/lfaure7/Documents/git/chapter-2/preparation donnee aigle ssf/donnees/emigration dates/emigration_dates_20250417.rds")
 
-# golden eagles with a date of dispersal
 rds_dir <- "C:/Users/lfaure7/Documents/git/chapter-2/preparation donnee aigle ssf/donnees/no_burst_GE/no_burst_GE"
 rds_files <- tools::file_path_sans_ext(list.files(rds_dir, pattern = "\\.rds$", ignore.case = TRUE))
 rds_ids   <- as.numeric(gsub("_gpsNoDup_moveObj", "", rds_files))
 
-# Keep only individuals with a confirmed dispersal date AND an RDS file
+# Filters individuals to retain those that have an emigration date
 emig_dates_filtered <- emig_dates[emig_dates$did_disperse == TRUE &
                                     emig_dates$individual.id %in% rds_ids, ]
 rds_files_filtered  <- rds_files[rds_ids %in% emig_dates_filtered$individual.id]
 
-cat("Individuals with confirmed dispersal date and RDS file:", nrow(emig_dates_filtered), "\n")
-
 #' # STEP 1 : filter the golden eagles data ------------------------------------
 #' (i) keep only GPS locations within the first 15 weeks after emigration date
-#' (ii) inspect speed and height to define filtering thresholds
-
-#' # STEP 1 : filter the golden eagles data ------------------------------------
-
+#' (ii) compute height above ground (geoid + DEM correction)
 dispersal_data <- lapply(rds_files_filtered, function(f) {
   
   id  <- as.numeric(gsub("_gpsNoDup_moveObj", "", f))
   obj <- readRDS(file.path(rds_dir, paste0(f, ".rds")))
   df  <- as.data.frame(obj)
   
-  # --- Filtrage des colonnes DÈS la lecture (comme ton ancien code) ---
+  # filter column
   df <- df[, c("individual.local.identifier", "timestamp",
                "location.long", "location.lat",
-               "height.above.ellipsoid", "ground.speed", 
-               "eobs.speed.accuracy.estimate", "eobs.horizontal.accuracy.estimate", 
-               "gps.dop", "height.above.ellipsoid", "vert.speed",
-               "turn.angle", "step.length", "gr.speed")]
+               "height.above.ellipsoid", "ground.speed",
+               "eobs.speed.accuracy.estimate", "eobs.horizontal.accuracy.estimate",
+               "gps.dop", "vert.speed",
+               "turn.angle", "step.length", "gr.speed", "eobs.type.of.fix")]
   
   df$timestamp <- as.POSIXct(df$timestamp, tz = "UTC")
   emig_dt <- emig_dates_filtered$dispersal_date[emig_dates_filtered$individual.id == id]
   
-  # Garde les 15 premières semaines (105 jours) après émigration
+  # retain only the first fifteen weeks of dispersal
   df <- df[df$timestamp >= emig_dt &
              df$timestamp <= emig_dt + 105 * 24 * 3600, ]
   
@@ -64,9 +59,31 @@ dispersal_data <- lapply(rds_files_filtered, function(f) {
   df$dispersal_date  <- emig_dt
   df$days_since_emig <- as.numeric(difftime(df$timestamp, emig_dt, units = "days"))
   
+  # calculate altitude (height above mean sea level) using the geoide
+  xy_ll <- as.matrix(df[, c("location.long", "location.lat")])
+  N     <- terra::extract(geo, xy_ll)[, 1]
+  
+  df$height_msl <- df$height.above.ellipsoid - N
+  
+  # applied the dem crs
+  pts     <- terra::vect(df[, c("location.long", "location.lat")],
+                         geom = c("location.long", "location.lat"),
+                         crs  = "EPSG:4326")
+  pts_dem <- terra::project(pts, crs(dem))
+  df$dem_elevation <- terra::extract(dem, pts_dem)[, 2]  
+  
+  # calculate flight height
+  df$height_above_ground <- df$height_msl - df$dem_elevation
+  
+  # filter the locations to remove all the point below -200m and above 3000m
+  df <- df[!is.na(df$height_above_ground) &
+             df$height_above_ground > -200 &
+             df$height_above_ground < 3000, ]
+  
   df
 })
 
 dispersal_data <- bind_rows(dispersal_data)
 rownames(dispersal_data) <- NULL
 
+sum(is.na(dispersal_data$dem_elevation))
