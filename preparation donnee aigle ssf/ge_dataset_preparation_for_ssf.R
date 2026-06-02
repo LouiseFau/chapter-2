3c98f86c9bec0c211c3bdff133fe783533346df1
LouiseFau <faurelouise91@yahoo.fr>
2026-06-02 13:21:46 +0200

    Define the objective of the code

---
+++ preparation donnee aigle ssf/ge_dataset_preparation_for_ssf.R
@@ -0,0 +1,8 @@
+#' ---
+#' title: "Preparing Golden Eagle dataset for ssf"
+#' author: "Louise Faure"
+#' date: 02.06.2026
+#' details: (i) select all the golden eagles gps values for the first 15 weeks of the dispersal phase, 
+#' (ii) define a threshold value to keep the location close to the group
+#' (iii) identify the time at which each location become independent    
+#' ---   
