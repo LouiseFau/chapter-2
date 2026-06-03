2e7c0a8367e5b135f620f934c1d89a6de10ecd90
LouiseFau <faurelouise91@yahoo.fr>
2026-06-03 15:21:02 +0200

    STEP 2 : sensibility check to identify a speed and height above the ground threshold to retain gps point that correspond to terrestrial behaviors.

--- preparation donnee aigle ssf/ge_dataset_preparation_for_ssf.R
+++ preparation donnee aigle ssf/ge_dataset_preparation_for_ssf.R
@@ -11,6 +11,7 @@
 #' #library
 library(dplyr)
 library(terra)
+library(move)
 
 #' # STEP 0 : load the data ----------------------------------------------------
 output_dir <- "C:/Users/lfaure7/Documents/git/chapter-2/preparation donnee aigle ssf/resultats-intermediaire"
@@ -31,7 +32,8 @@ emig_dates_filtered <- emig_dates[emig_dates$did_disperse == TRUE &
                                     emig_dates$individual.id %in% rds_ids, ]
 rds_files_filtered  <- rds_files[rds_ids %in% emig_dates_filtered$individual.id]
 
-#' # STEP 1 : filter the golden eagles data ------------------------------------
+
+#' # STEP 1 : filtration and calculation of height values
 #' (i) keep only GPS locations within the first 15 weeks after emigration date
 #' (ii) compute height above ground (geoid + DEM correction)
 dispersal_data <- lapply(rds_files_filtered, function(f) {
@@ -79,11 +81,35 @@ dispersal_data <- lapply(rds_files_filtered, function(f) {
   df <- df[!is.na(df$height_above_ground) &
              df$height_above_ground > -200 &
              df$height_above_ground < 3000, ]
-  
+
   df
 })
 
 dispersal_data <- bind_rows(dispersal_data)
 rownames(dispersal_data) <- NULL
 
-sum(is.na(dispersal_data$dem_elevation))
+#'STEP 2 : sensibility test to identify location points associated with terrestrial behaviors (resting, walking, running)
+#'(i) identification of a speed threshold 
+#'(ii) identification of an height above the ground threshold
+grid <- expand.grid(v = c(1.5, 2, 3), h = c(30, 50, 100))
+grid$n <- mapply(function(v, h)
+  sum(dispersal_data$ground.speed < v & dispersal_data$height_above_ground < h, na.rm = TRUE),
+  grid$v, grid$h)
+grid 
+
+# We find that at constant height, the nbr of gps point does not change substantially, while at constant speed, the nbr of gps points varies.
+# We conclude that the threshold of speed has little influenced on the selection of location points. 
+# To define a height threshold, we explore the dispersal of height values for location that are certainly stationary (e.i., < 0.5 m/s)
+
+dispersal_data %>%
+  filter(ground.speed < 0.5) %>%  
+  pull(height_above_ground) %>%
+  quantile(c(0.5, 0.9, 0.95, 0.99), na.rm = TRUE) # 90% of height values are below 59m, we can therefore keep the height threshold at 50m
+
+dispersal_terrestrial <- dispersal_data %>%
+  filter(!is.na(ground.speed),
+         ground.speed        < 2,
+         height_above_ground < 50)
+
+#'STEP 3: identify the time at which each location point become independant 
+#'
